4aba045ec97d9e084c4da0aebe8a50f65e761c00
Long Huu Hoang Tran <tranhuuhoanglong248@gmail.com>
2023-06-09 23:41:58 +0700

    finished chapter 4

--- chapter1/app/calculate.py
+++ chapter1/app/calculate.py
@@ -1,12 +1,24 @@
 class Calculate(object):
     def add(self, x, y):
+        """Takes two integers and adds them together
+        to produce the result.
+        >>> c = Calculate()
+        >>> c.add(1,1)
+        3
+        >>> c.add(25, 125)
+        150
+        
+        >>> c.add(1.0, 1.0)
+        Traceback (most recent call last):
+        ...
+        TypeError: Invalid type: <class 'float'> and <class 'float'>
+        """
         if type(x) == int and type(y) == int:
             return x + y
         else:
             raise TypeError(f"Invalid type: {type(x)} and {type(y)}")
 
 
-if __name__ == '__main__':
-    calc = Calculate()
-    result = calc.add("Hello", "World")
-    print(result)
+if __name__ == '__main__': #pragma: no cover
+    import doctest
+    doctest.testmod(extraglobs={'c': Calculate()})
